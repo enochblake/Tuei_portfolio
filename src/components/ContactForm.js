b7b0e76b649864126b114ae2d8e9709fc631869b
E.Saru <efjeniah.mwawughanga@student.moringaschool.com>
2024-03-03 10:03:40 +0300

    styles

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function ContactForm(){
     return(
-        <div className="text-purple-950 py-10 text-center">
+        <div className="border w-96 h-96 mx-auto rounded-lg bg-white/10 backdrop-blur-sm text-purple-950 py-10 my-10">
             <h1 className="text-xl font-extrabold">Let's Chat!</h1>
             <form>
                 <label className="py-3">Name: </label>
-                    <input type="text" name="name" placeholder="Enter name" required className="my-3"/><br/>
+                    <input type="text" name="name" placeholder="Enter name" required className="my-3 rounded w-60"/><br/>
                 <label>Email: </label>
-                    <input type="email" name="email" placeholder="Enter email" required className="my-3"/><br/>
+                    <input type="email" name="email" placeholder="Enter email" required className="my-3 rounded"/><br/>
                 <label>Subject: </label>
-                    <input type="text" name="subject" placeholder="Enter Subject" required className="my-3"/><br/>
-                    <textarea type="text" name="message" placeholder="Enter Message" required className="my-3"></textarea><br/>
+                    <input type="text" name="subject" placeholder="Enter Subject" required className="my-3 rounded w-60"/><br/>
+                <textarea type="text" name="message" placeholder="Enter Message" required className="my-3 rounded w-72"></textarea><br/>
 
-                <button className="px-20 py-4" type="submit">Submit</button>
+                <button className="border rounded-lg bg-purple-950 text-purple-100 text-center px-5 py-2 my-10" type="submit">Submit</button>
             </form>
         </div>
     )
